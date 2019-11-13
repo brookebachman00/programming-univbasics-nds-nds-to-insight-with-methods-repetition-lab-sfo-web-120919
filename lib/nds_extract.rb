c217acc8b235d6ad64eb8a55dff037fc48d11bbf
Brooke Bachman <bbachman1994@gmail.com>
2019-11-13 04:19:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,9 +25,10 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
+  directors = []
   source.each do |director|
-    directors += source[:name]
-    directors = []
+    directors << source[:name]
+
   end
 
   # Write this implementation
