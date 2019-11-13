f7b5c0983031ea0659d1c0636bde0e6136330776
Brooke Bachman <bbachman1994@gmail.com>
2019-11-13 04:15:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
 require 'pry'
+
 def directors_totals(source)
   result = {}
   director_index = 0
@@ -35,7 +36,9 @@ end
 
 def total_gross(source)
   # Write this implementation
-  total = directors_totals(director)
+  total = 0
+  total += directors_totals[director_name] 
+  binding.pry
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
   # 2. list_of_directors: names provides an Array of directors names (use
