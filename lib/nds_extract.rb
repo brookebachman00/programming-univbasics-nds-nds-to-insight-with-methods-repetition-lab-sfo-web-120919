fa45fa9792521b80d5ccdee12963383b8d877363
Brooke Bachman <bbachman1994@gmail.com>
2019-11-13 04:31:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,8 +37,11 @@ end
 
 def total_gross(source)
   # Write this implementation
-  total = directors_totals(source)
+  list = directors_totals(source)
   names = list_of_directors(source)
+  names.each do |name|
+    list[name]
+  end
   binding.pry
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
